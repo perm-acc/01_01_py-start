cb99cf652b48da61c370da8602656de3d3d0a7ab
Tema <artem.k.it.s@gmail.com>
2020-09-13 19:35:24 +0300

    task 2, version 2

---
+++ task2_v2.py
@@ -0,0 +1,3 @@
+def card(**kwargs):
+    return kwargs
+print(card(name = input('Bведите имя: '), surname = input('Введите фамилию: '), b_year = input('Введите год рождения: '), city = input('Введите город проживания: '), email = input('Введите электронную почту: '), p_num = input('Введите номер телефона: ')))
